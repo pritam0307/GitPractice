994dc8770963727606730651aa74e8bab0cac9fc
pritam0307 <pritam.bhat1@gmail.com>
2022-08-27 10:39:43 +0530

    first commit

---
+++ src/pritam/Test.java
@@ -0,0 +1,10 @@
+package pritam;
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is inside test");
+
+	}
+
+}
---
+++ src/pritam/Test2.java
@@ -0,0 +1,10 @@
+package pritam;
+
+public class Test2 {
+	
+	public void test()
+	{
+		System.out.println("This is a method");
+	}
+
+}
